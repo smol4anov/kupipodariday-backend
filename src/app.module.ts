976e4a0fcc27bca66b26cf63898dc89a7b18a0d6
Sergey Molchanov <smol4anov@yandex.ru>
2023-12-02 11:46:06 +0300

    creat API framework

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] })],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), UsersModule, WishesModule, WishlistsModule, OffersModule],
   controllers: [AppController],
   providers: [],
 })
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,7 @@ export default () => ({
   database: {
     url: process.env.DATABASE_URL,
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    user: process.env.DATABASE_USER || test,
-    password: process.env.DATABASE_PASSWORD || test,
+    user: process.env.DATABASE_USER || 'test',
+    password: process.env.DATABASE_PASSWORD || 'test',
   },
 });
